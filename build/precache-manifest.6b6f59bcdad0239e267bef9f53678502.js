e22d7dbe4abde9299670c8ab0970c85e6a0ce950
Bill Li <billmli88@gmail.com>
2021-09-05 03:31:19 -0700

    social image

---
+++ build/precache-manifest.6b6f59bcdad0239e267bef9f53678502.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed9f2e49dbe799c26cda464e8ce1983",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff674b33e43e2f801909",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff674b33e43e2f801909",
+    "url": "/static/js/main.d846d241.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "c2223018e4d6ee33527b9a789bf7d68d",
+    "url": "/static/media/Bill Li Resume.c2223018.pdf"
+  },
+  {
+    "revision": "ced95a22ccf05fd9a401b30ea0b0309c",
+    "url": "/static/media/bill.ced95a22.jpg"
+  },
+  {
+    "revision": "2e3a086e4029eac0cbbc41218b1d8033",
+    "url": "/static/media/ibm.2e3a086e.png"
+  },
+  {
+    "revision": "c2223018e4d6ee33527b9a789bf7d68d",
+    "url": "/static/media/resume.c2223018.pdf"
+  }
+]);
