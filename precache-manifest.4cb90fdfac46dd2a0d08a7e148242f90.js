e94bbde9604c9e95dda38830244870cd07b351e0
Bill Li <billmli88@gmail.com>
2021-09-06 02:46:44 -0700

    Updates

---
+++ precache-manifest.4cb90fdfac46dd2a0d08a7e148242f90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba16d287bba34a7d06bed6bed141da42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cf1a981e034fe8784a7",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "a3b1a683b0dcdf6d6db7",
+    "url": "/static/js/2.6d125ced.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6d125ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf1a981e034fe8784a7",
+    "url": "/static/js/main.21551f90.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "ced95a22ccf05fd9a401b30ea0b0309c",
+    "url": "/static/media/bill.ced95a22.jpg"
+  },
+  {
+    "revision": "2e3a086e4029eac0cbbc41218b1d8033",
+    "url": "/static/media/ibm.2e3a086e.png"
+  },
+  {
+    "revision": "c2223018e4d6ee33527b9a789bf7d68d",
+    "url": "/static/media/resume.c2223018.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.a06396c3481e617be70cf519a1a1365a.js"
+  "/precache-manifest.4cb90fdfac46dd2a0d08a7e148242f90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
